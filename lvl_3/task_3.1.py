9bd97b965c347c32ad2c1b04d4829569499cf6d3
PelevinaSofia <sofia.pelevina@mail.ru>
2023-05-28 00:07:44 +0700

    lvl_3 done

--- lvl_3/task_3.1.py
+++ lvl_3/task_3.1.py
@@ -24,4 +24,38 @@
 #   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
 #   - отображать в таблице/матрице название колонки не обязательно!
 #   - проявите фантазию :)
+from pprint import pprint
 
+class Matrix:
+
+    def __init__(self, x, y, n):
+        self.x = x
+        self.y = y
+        self.matrix = [[n for j in range(x)] for i in range(y)]
+
+    def print_m(self):
+        matrix = self.matrix
+        for im in range(len(matrix)):
+            print(matrix[im])
+    
+    def count(self):
+        matrix=self.matrix
+        x = len(matrix)
+        for sublist in matrix:
+            y = len(sublist)
+        print("Число строк:", x, "число колонок:", y)
+
+    def new_value(self, value):
+        self.value = value
+        matrix = self.matrix
+        for index, item in enumerate(matrix):
+            item = self.value
+            matrix = [[index for item in range(len(matrix))] for item in range(len(matrix))]
+        pprint(matrix)
+
+       
+
+m = Matrix(10, 10, 1)
+m.print_m()
+m.count()
+m.new_value(9)
